94a4ad7c278ef1c6cb43a43f1c9e60ddedbfa6f2
Julio Orozco <ingjulio20@gmail.com>
2025-10-10 19:35:51 -0500

    git commit -m "Cambios en Templates Usuarios"

--- app/routes/usuarios_routes.py
+++ app/routes/usuarios_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_bcrypt import Bcrypt
 import mysql.connector.errors as error
+from app.services import usuarios_service
 
 #Blueprint
 bp_usuarios = Blueprint('usuarios', __name__)
@@ -10,3 +11,8 @@ bcrypt = Bcrypt()
 @bp_usuarios.get('/usuarios')
 def usuarios():
     return render_template('tmp_usuarios/usuarios.html')
+
+@bp_usuarios.get('/nuevo_usuario')
+def nuevo_usuario():
+    perfil = usuarios_service.listar_perfiles_usuario()
+    return render_template('tmp_usuarios/nuevo_usuario.html', perfil = perfil)
