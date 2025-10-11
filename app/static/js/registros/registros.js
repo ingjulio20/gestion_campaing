52e05f05f1a50a27726a95e7d10729d59780d51f
Julio Orozco <ingjulio20@gmail.com>
2025-10-11 16:26:43 -0500

    "Finalizando Routes de registros"

---
+++ app/static/js/registros/registros.js
@@ -0,0 +1,17 @@
+/* DataTable */
+let tableRegistros = new DataTable('#tablaRegistros', {
+  language: {
+    lengthMenu: "Mostrar _MENU_ registros por pagina",
+    zeroRecords: "Sin registros encontrados",
+    info: "Mostrando pagina _PAGE_ de _PAGES_",
+    infoEmpty: "No hay registros disponibles",
+    infoFiltered: "(filtrado de _MAX_ registros)",
+    search: "Filtrar:",
+    paginate: {
+      first: "Primera",
+      last: "Última",
+      next: "Siguiente",
+      previous: "Anterior"
+    }
+  }
+});
